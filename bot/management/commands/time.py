6824bc3cdb43c2dd8a0a318c33c08026a62856cf
kirill <starshinin-kirill@rambler.ru>
2023-04-14 14:44:41 +0300

    add to origin field chanel

---
+++ bot/management/commands/time.py
@@ -0,0 +1,18 @@
+import time
+# from datetime import *
+#
+# print(datetime.now())
+
+
+named_tuple = time.localtime()
+start_time = '00:00:00'
+end_time = '23:30:00'
+now = time.strftime("%H:%M:%S", time.localtime())
+print(f'start: {start_time}, end: {end_time}, now:{now}')
+#
+# if start_time <= now <= end_time:
+#     print('Время в диапазоне')
+# elif start_time == end_time:
+#     print('Работает круглосуточно')
+# else:
+#     print('Время вне диапазона')
